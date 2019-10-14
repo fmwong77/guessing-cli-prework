6b6a33c915f85597da62c7dcdc5d5e9cff918141
Mei Wong <fongmeiw@gmail.com>
2019-10-14 15:34:20 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,13 @@
 # Code your solution here!
 require 'pry'
+def prompt_user
+  puts "Guess a number between 1 and 6."
+end
+
+def get_user_input
+  gets.chomp
+end
+
 def run_guessing_game
   binding.pry
   random_number = rand(6) + 1
@@ -18,10 +26,4 @@ def run_guessing_game
   end
 end
 
-def prompt_user
-  puts "Guess a number between 1 and 6."
-end
 
-def get_user_input
-  gets.chomp
-end
