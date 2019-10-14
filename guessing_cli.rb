7b08e327a2c5b1473651d374a8158df7ed6917fa
Mei Wong <fongmeiw@gmail.com>
2019-10-14 15:28:30 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,8 +2,8 @@
 require 'pry'
 def run_guessing_game
   random_number = rand(6) + 1
-  prompt_user()
-  user_input = get_user_input()
+  prompt_user
+  user_input = get_user_input
   
   if user_input.downcase == "exit"
     puts "Goodbye!"
