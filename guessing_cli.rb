826a08bb905affbb437d99bf4af223a7cfe054ee
Mei Wong <fongmeiw@gmail.com>
2019-10-14 15:37:14 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -15,7 +15,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   # prompt_user
   user_input = get_user_input
-  
+  binding.pry
   if user_input.downcase == "exit"
     puts "Goodbye!"
   elsif user_input != random_number.to_s
