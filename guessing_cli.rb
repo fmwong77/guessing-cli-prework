8bc2030d85c53b3e7c4188730228ee84506fecd4
Mei Wong <fongmeiw@gmail.com>
2019-10-14 15:05:32 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,6 @@
 def run_guessing_game
   random_number = rand(6) + 1
   prompt_user()
-  # puts "Guess a number between 1 and 6."
   user_input = get_user_input()
   if user_input == "exit"
     puts "Goodbye!"
